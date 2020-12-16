c6a68785566b432b2817824874bd81ad46e5ee66
Adam Cox <mr.adamcox@gmail.com>
2020-12-15 19:18:07 -0600

    big cleanup

--- ifc-map/map.js
+++ ifc-map/map.js
@@ -1,20 +1,13 @@
 var MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYWNveDQyIiwiYSI6ImNrZjVtd3ZiejBvYnkyeW9nZnB4MzVva3EifQ.COv27lSO4vobAVLLHNVkQg'
-var BASE_STYLE = 'mapbox://styles/acox42/ckg9uw0dq6xp019petpmu77yf'
-var BASE_STYLE = 'mapbox://styles/legiongis/ckg13im88155l19pdgv294lu0'
-// var BASE_STYLE = 'mapbox://styles/mapbox/satellite-streets-v11'
-// var BASE_STYLE = 'mapbox://styles/acox42/ckg9x9i0b2smo18mkero6jvm3'
-var BASE_STYLE = 'mapbox://styles/acox42/ckgtxvv580c9w19kx5gwzc47c'
 // lindsey's style on her account
 var BASE_STYLE = 'mapbox://styles/l-taylor9/ckigllktr5a7p19p95nj9nt7k'
 
 // some map behavior constants
 var INITIAL_BOUNDS = [-91.49, 30, -90.15, 31.03]
 var WELCOME_TEXT = "<em>use the buttons below to explore the communities we have worked with...</em>"
-var CAP_REGION_BOUNDS = [-91.41, 29.99, -90.44, 30.74];
 
-var AMITE_BOUNDS = [-91.67, 29.89, -89.95, 31.21];
 var AMITE_TEXT = "Within the Amite River watershed, we ....."
-
+var AMITE_BOUNDS = [-91.67, 29.89, -89.95, 31.21];
 
 // mapbox tileset uris and label names
 var HUC6_MB_SRC = 'mapbox://acox42.dhw9cd9r';
@@ -34,21 +27,14 @@ var IFC_STUDIO_FILL = ['rgba', 101, 198, 210, .2];
 var IFC_PARTNER = ['rgb', 145, 139, 195];
 var IFC_PARTNER_FILL = ['rgba', 145, 139, 195, .2];
 
+}
+
+
+
 
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
-
-function freezeBounds() {
-  map.setMaxBounds(map.getBounds());
-  map.setMinZoom(map.getZoom());
-}
-
-function setText(text) {
-  $("#description-box").html(`<p>${text}</p>`);
-}
-setText(WELCOME_TEXT);
-
 function fadeLayers(direction, layers, maxOpacityPercent) {
   if (maxOpacityPercent === undefined) {maxOpacityPercent = 100}
   for(var i = 0; i < maxOpacityPercent + 1; i++){
@@ -61,6 +47,18 @@ function fadeLayers(direction, layers, maxOpacityPercent) {
     })
   };
 }
+
+
+function freezeBounds() {
+  map.setMaxBounds(map.getBounds());
+  map.setMinZoom(map.getZoom());
+}
+
+function setText(text) {
+  $("#description-box").html(`<p>${text}</p>`);
+}
+
+
 
 function setCommunitiesToYellow() {
   map.setPaintProperty('communities-fill', 'fill-color', IFC_CSS_FILL);
@@ -77,14 +75,15 @@ function setCommunitiesToOrange() {
 // different maps for different iframes.
 // <iframe name="full" src="..."></iframe>
 var showRegion;
-var pageType;
+var initializedAs;
 if (window.name == "") {
+  setText(WELCOME_TEXT);
   showRegion = "initial-extent";
-  pageType = "mainPage";
+  initializedAs = "mainPage";
 
 } else {
   showRegion = window.name;
-  pageType = "communityPage";
+  initializedAs = "communityPage";
   // $(".top-bar").hide();
   $(".bottom-bar").hide();
   // $("#map").css("border-radius", "5px")
@@ -137,13 +136,12 @@ var buttonZoom = function (zoomTo) {
   map.setMaxBounds(null);
   map.setMinZoom(0);
 
-  $(".loc-button").removeClass("active")
-
   // this allows a zoom id to be passed from either a clicked button or
   // clicked feature.
   var id;
   if (typeof this.id === 'undefined') { id = zoomTo; } else { id = this.id; }
 
+  $(".loc-button").removeClass("active")
   $("#"+id).addClass("active")
   showRegion = id;
 
@@ -164,21 +162,6 @@ var buttonZoom = function (zoomTo) {
       fadeLayers('out', ['study-areas-line', 'study-areas-fill', 'communities-mask'])
       fadeLayers('in', ['communities-line', 'communities-fill'])
       break
-
-    // case "watershed-extent":
-      // wsdFadeDir = "in";
-      // mskFadeDir = "out";myVar
-      // comFadeDir = "out";
-      //
-      // setText(watershedText);
-      //
-      // bounds = [
-      //   amiteFeature.properties.x1,
-      //   amiteFeature.properties.y1,
-      //   amiteFeature.properties.x2,
-      //   amiteFeature.properties.y2,
-      // ];
-      // break
 
     default:
 
@@ -217,9 +200,6 @@ var buttonZoom = function (zoomTo) {
 
   // freeze the map once the zoom is completed.
   map.once('moveend', freezeBounds);
-
-  // fadeLayers(wsdFadeDir,['huc6-lyr', 'huc8-lyr', 'huc10-lyr', 'amite-lyr', 'amite-lyr-white'],'line', 75)
-  // fadeLayers(wsdFadeDir,['huc8-lyr-fill'],'fill', 75)
 }
 
 // acquire the geojson features for
@@ -236,16 +216,6 @@ $.getJSON(communitiesJsonFile, function(data) {
   $(".loc-button").click(buttonZoom);
   $(".loc-button").hover(buttonHover);
 });
-
-// acquire the geojson features for
-// var amiteFeature;
-// var amiteJson = "AmiteWatershed.geojson"
-// $.getJSON(amiteJson, function(data) {
-//   data['features'].forEach(function(feat) {
-//     feat.id = feat.properties.id
-//     amiteFeature = feat;
-//   })
-// });
 
 map.on('load', function() {
 
@@ -405,194 +375,9 @@ map.on('load', function() {
 
   });
 
-  map.on('mousemove', 'communities-fill', function (e) {
-    // console.log(e)
-    // if (currentId !== e.features[0].properties.id) {
-    //   currentId = e.features[0].properties.id;
-    //   console.log(currentId);
-    // }
-
-    // buttonHover(e.features[0].properties.id)
-    // popup.remove();
-  });
-
-
-
   map.on('click', 'communities-fill', function (e) {
-
     buttonZoom(e.features[0].properties.id)
   });
-
-  // for the subset pages, zoom to the specified extent
-  if (showRegion != "initial-extent"){ buttonZoom(showRegion); }
-
-
-  // these are all of the watershed sources
-  // note urls are all defined at top of file
-  // map.addSource('huc6', { type: "vector", url: HUC6_MB_SRC });
-  // map.addSource('huc8', { type: 'vector', url: HUC8_MB_SRC });
-  // map.addSource('huc10', { type: 'vector', url: HUC10_MB_SRC });
-
-  // definition of all watershed layers.
-  // note source-layer is defined at top of file
-  var huc8Lyr = {
-    'id': 'huc8-lyr',
-    'type': 'line',
-    'source': 'huc8',
-    'source-layer': HUC8_MB_LYR,
-    'paint': {
-      'line-color': [
-        "interpolate",
-        ["linear"],
-        ["zoom"],
-        0,
-        "hsla(293, 74%, 31%, 0.19)",
-        18,
-        "hsl(293, 74%, 31%)"
-      ],
-      "line-width": 1.5,
-      "line-dasharray": [2, 2],
-      'line-opacity': 1,
-    },
-    'layout': {
-     'line-cap': 'round',
-     'line-join': 'round',
-    }
-  };
-//
-//   var huc8LyrFill = {
-//     'id': 'huc8-lyr-fill',
-//     'type': 'fill',
-//     'source': 'huc8',
-//     'source-layer': 'HU8Boundaries-7m6k8g',
-//     'paint': {
-//       'fill-color': [
-//         'case',
-//         ["!=", ['get', 'Name'], "Amite"],
-//         ['rgba', 0, 0, 0, .5],
-//         ['rgba', 0, 0, 0, 0]
-//       ],
-//       'fill-opacity': 0,
-//     }
-//   };
-//
-
-//
-  var amiteLyrWhite = {
-    'id': 'amite-lyr-white',
-    'type': 'line',
-    'source': 'huc6',
-    'source-layer': HUC6_MB_LYR,
-    'paint': {
-      "line-color": "#ffffff",
-      "line-width": 3,
-      "line-opacity": 1,
-    },
-    'layout': {
-     'line-cap': 'round',
-     'line-join': 'round',
-    }
-  };
-  var amiteLyr = {
-    'id': 'amite-lyr',
-    'type': 'line',
-    'source': 'huc6',
-    'source-layer': HUC6_MB_LYR,
-    'paint': {
-      "line-color": [
-        "interpolate",
-        ["linear"],
-        ["zoom"],
-        2,
-        "hsla(263, 80%, 24%, 0.8)",
-        18,
-        "hsla(263, 80%,000000 24%, 0.56)"
-      ],
-      "line-dasharray": [2, 2],
-      "line-width": 3,
-      "line-opacity": 1,
-    },
-    'layout': {
-     'line-cap': 'round',
-     'line-join': 'round',
-    }
-  };
-
-
-
-  var huc10Lyr = {
-    'id': 'huc10-lyr',
-    'type': 'line',
-    'source': 'huc10',
-    'source-layer': HUC10_MB_LYR,
-    'paint': {
-      'line-color': [
-        "interpolate",
-        ["linear"],
-        ["zoom"],
-        5,
-        "hsla(290, 67%, 39%, 0)",
-        18,
-        "hsl(290, 67%, 39%)"
-      ],
-      'line-width': 1,
-      "line-dasharray": [2, 2],
-      'line-opacity': 1,
-    },
-    'layout': {
-     'line-cap': 'round',
-     'line-join': 'round',
-    }
-  };
-
-  // map.addLayer(huc10Lyr);
-  // map.addLayer(huc8Lyr);
-  // map.addLayer(amiteLyrWhite);
-  // map.addLayer(amiteLyr);
-
-  // map.addLayer({
-  //   'id': 'cities-hover',
-  //   "type": "fill",
-  //   "source": "study-areas",
-  //   "paint": {
-  //     'fill-color': '#627BC1',
-      // 'fill-opacity': [
-      //   'case',
-      //   ['boolean', ['feature-state', 'hover'], false],
-      //   .1,
-      //   0
-      // ]
-  //   }
-  // })
-
-
-  // map.on('mouseenter', 'cities-hover', function (e) {
-  //
-  //   if (e.features.length > 0) {
-  //     if (hoveredStateId) {
-  //       map.setFeatureState(
-  //           { source: 'study-areas', id: hoveredStateId },
-  //           { hover: false }
-  //         );
-  //       }
-  //       hoveredStateId = e.features[0].id;
-  //       map.setFeatureState(
-  //         { source: 'study-areas', id: hoveredStateId },
-  //         { hover: true }
-  //       );
-  //     }
-  //
-  // });
-
-  // map.on('mouseleave', 'cities-hover', function () {
-  //   if (hoveredStateId) {
-  //     map.setFeatureState(
-  //       { source: 'study-areas', id: hoveredStateId },
-  //       { hover: false }
-  //     );
-  //   }
-  //   hoveredStateId = null;
-  // });
 
   map.on('mouseenter', 'study-areas-fill', function (e) {
 
@@ -623,39 +408,5 @@ map.on('load', function() {
       popup.remove();
     }
   });
-
-  // map.on('mouseleave', 'cities-hover', function () {
-  //   map.getCanvas().style.cursor = '';
-    // popup.remove();
-  // });
-
-  // var compositeSrc = {
-  //     "composite": {
-  //         "url": "mapbox://acox42.983n0sb2,mapbox.mapbox-streets-v8,acox42.dhw9cd9r,acox42.8221d82f,acox42.3ak5cakw,mapbox.mapbox-terrain-v2",
-  //         "type": "vector"
-  //     }
-  // },
-
-  // map.addSource('parishCentroids', {
-  //     type: 'vector',
-  //     url: PARISH_CENTROIDS_MB_SRC
-  //   });
-  //
-  // var parishLabelsLyr = {
-  //     "id": "allparishes-centroids-cd7pvh",
-  //     "type": "symbol",
-  //     "source": "parishCentroids",
-  //     "source-layer": PARISH_CENTROIDS_MB_LYR,
-  //     "layout": {
-  //         "text-field": ["to-string", ["get", "NAMELSAD"]],
-  //         "text-font": ["DIN Pro Italic", "Arial Unicode MS Regular"],
-  //         "text-letter-spacing": 0.15,
-  //         "text-size": 14,
-  //         "text-allow-overlap": true
-  //     },
-  //     "paint": {}
-  // }
-
-  // map.addLayer(parishLabelsLyr);
 
 });
